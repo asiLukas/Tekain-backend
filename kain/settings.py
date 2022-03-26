cf59fdf8646f44ed53b16084eb2429c4e48f75d8
asiLukas <lukas.zagorsky@seznam.cz>
2022-03-26 18:46:10 +0100

    asf

--- kain/settings.py
+++ kain/settings.py
@@ -83,7 +83,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [('redis', 6379)],
         },
     },
 }
