e4521605044c8e3d6d5ce5a5635a778a883116be
asiLukas <lukas.zagorsky@seznam.cz>
2022-03-26 18:12:34 +0100

    v1

--- kain/asgi.py
+++ kain/asgi.py
@@ -7,7 +7,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kain.settings')
 django_asgi_app = get_asgi_application()
-
+django.setup()
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
