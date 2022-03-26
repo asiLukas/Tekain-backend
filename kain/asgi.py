4a240ec66891b821f001a9abb3629a050ad283cf
asiLukas <lukas.zagorsky@seznam.cz>
2022-03-26 18:08:35 +0100

    v1

--- kain/asgi.py
+++ kain/asgi.py
@@ -1,7 +1,18 @@
 import os
 import django
-from channels.routing import get_default_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+import chat.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kain.settings')
-django.setup()
-application = get_default_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
