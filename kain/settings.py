68ab82d584bb526707de9c17871e2b38f9796154
asiLukas <lukas.zagorsky@seznam.cz>
2022-03-26 18:48:59 +0100

    asdfa

--- kain/settings.py
+++ kain/settings.py
@@ -83,7 +83,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('redis', 6379)],
+            "hosts": [('redis', 6319)],
         },
     },
 }
